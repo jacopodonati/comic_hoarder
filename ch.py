f58a85fd3c4aed713cdd67cd4e72723f48a175be
Jacopo Donati <33698919+jacopodonati@users.noreply.github.com>
2024-02-06 13:48:24 +0100

    Abstract the download mechanism

--- ch.py
+++ ch.py
@@ -17,80 +17,28 @@
 
 import argparse
 import logging
+from urllib.parse import urlparse
 
 from platforms import *
 
 PAGE_NOT_SUPPORTED = 0
+PAGE_ERROR = -1
 PAGE_ARCHIVE = 1
 PAGE_SINGLE = 2
 
-settings = {}
-
-def download_archive(url):
-    logging.debug(f"Downloading {settings['quantity']} comic from archive at {url}")
-    headers = { 
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36'
-        }
-    page = requests.get(url, headers=headers)
-    if (page.status_code == 200):
-        tree = html.fromstring(page.text)
-        o = urlparse(url)
-        latest_path = tree.xpath('//span[text()="Read Now"]/ancestor::div/ancestor::div/ancestor::a/@href')
-        latest_url = urlunparse(o._replace(path=latest_path[0]))
-        for i in range(settings['quantity']):
-            logging.debug(f"Latest URL is {latest_url}.")
-            download_single(latest_url)
-            if i < settings['quantity']:
-                page = requests.get(latest_url, headers=headers)
-                tree = html.fromstring(page.text)
-                latest_path = tree.xpath('//a[contains(@class, "js-previous-comic")]/@href')
-                latest_url = urlunparse(o._replace(path=latest_path[0]))
-
-def download_single(url):
-    logging.debug(f"Downloading single comic at {url}")
-    headers = { 
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36'
-        }
-    page = requests.get(url, headers=headers)
-    if (page.status_code == 200):
-        tree = html.fromstring(page.text)
-        comic_title = tree.xpath('//div[@data-shareable-model="FeatureItem"]/@data-feature-name')
-        comic_date = tree.xpath('//div[@data-shareable-model="FeatureItem"]/@data-date')
-        img_url = tree.xpath('//picture[@class="item-comic-image"]/img/@src')
-        if not settings['dry']:
-            logging.debug(f"Downloading image {img_url[0]}")
-            r = requests.get(img_url[0])
-            path = './'
-            filename = f"{comic_title[0]} - {comic_date[0]}.gif"
-            file = os.path.join(path, filename)
-            if os.path.exists(file):
-                logging.debug(f"File {file} already exists. Skipping.")
-            else:
-                with open(file, 'wb') as fd:
-                    logging.debug(f"Writing file {filename} in folder {path}")
-                    fd.write(r.content)
-        else:
-            logging.debug(f"Simulating the download of image {img_url[0]}")
-    else:
-        logging.error(f"Error downloading {url}. Status code: {page.status_code}")
-
-def identify_url(url):
-    logging.debug(f"Checking what kind of URL is {url}")
+def get_platform(url):
+    logging.debug(f"Checking which platform hosts the comic")
+    platforms = {
+        'www.gocomics.com': GoComics}
     o = urlparse(url)
-    re_archive = re.compile('/[a-zA-Z0-9-]+')
-    re_single = re.compile('/[a-zA-Z0-9-]+/[0-9]+/[0-9]+/[0-9]+')
-    if o.hostname != 'www.gocomics.com':
+    if o.hostname in platforms:
+        p = platforms[o.hostname]
+        logging.debug(f"URL is hosted on {p.name}")
+        return p
+    else:
         logging.debug('URL not supported')
         return PAGE_NOT_SUPPORTED
-    elif re_archive.fullmatch(o.path):
-        logging.debug('URL is an archive')
-        return PAGE_ARCHIVE
-    elif re_single.fullmatch(o.path):
-        logging.debug('URL is a single page')
-        return PAGE_SINGLE
-    else:
-        logging.debug('URL error')
-        return PAGE_ERROR
+
 
 def main():
     # Define the args you can pass
@@ -110,7 +58,7 @@ def main():
                         help='Simulate the download without downloading any file'
                         )
     parser.add_argument('--quantity',
-                        nargs='?',
+                        nargs=1,
                         default=1,
                         type=int,
                         help='How many comics to download from an archive. Default is 1 (i.e. the latest).'
@@ -130,11 +78,13 @@ def main():
     settings['dry'] = args.dry
     settings['quantity'] = args.quantity[0]
     logging.debug(f"Downloading {settings['quantity']} comics from {settings['url']}.")
-    settings['kind_of_page'] = identify_url(settings['url'])
-    if (settings['kind_of_page'] == PAGE_SINGLE):
-        download_single(settings['url'])
-    elif (settings['kind_of_page'] == PAGE_ARCHIVE):
-        download_archive(settings['url'])
+    platform = get_platform(settings['url'])
+    if platform != PAGE_NOT_SUPPORTED:
+        settings['kind_of_page'] = platform.identify_url(url=settings['url'])
+        if (settings['kind_of_page'] == PAGE_SINGLE):
+            platform.download_single(url=settings['url'], dry=settings['dry'])
+        elif (settings['kind_of_page'] == PAGE_ARCHIVE):
+            platform.download_archive(url=settings['url'], quantity=settings['quantity'], dry=settings['dry'])
 
 if __name__ == "__main__":
     main()
